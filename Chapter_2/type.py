d431c893eef1a0311b4bd3156eb8f173ea51caf3
Abhishek Mittal <mittalabhishek667@gmail.com>
2025-05-10 22:13:17 +0530

    learning_type

---
+++ Chapter_2/type.py
@@ -0,0 +1,12 @@
+x = 10
+print(type(x))  # Output: <class 'int'>
+
+
+y = "hello"
+print(type(y))  # Output: <class 'str'>
+
+z = 10.3
+print(type(z))  # Output: <class 'float'>
+
+t = True
+print(type(t))  # Output: <class 'bool'>
